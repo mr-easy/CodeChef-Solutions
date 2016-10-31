b4c900a9a4b462a50ecadd2dc8a95d5bea546320
Rishabh Gupta <rishabhg1997@gmail.com>
2016-10-31 23:15:37 +0530

    Update
    New solutions added.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CodeChef-Solutions
-Solutions to some awesome problems from codechef.
+Solutions of some selected problems of <a href="www.codechef.com">codechef</a>.
---
+++ TEST.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    while(1)
+    {
+        cin>>n;
+        if(n==42)
+            break;
+        cout<<n<<"\n";
+    }
+    return 0;
+}
